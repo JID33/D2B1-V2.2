3e0d7368c97858855feacd4cc4ef4eeb1adbaafa
JID33 <emmanueljeambas@gmail.com>
2025-06-27 07:54:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Système D2B1 initialisé');
+function envoyerVersGoogleSheet(donnees) {
+  fetch('https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec', {
+    method: 'POST',
+    body: JSON.stringify(donnees),
+    headers: { "Content-Type": "application/json" }
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log("Données envoyées à Google Sheets :", data.message);
+  })
+  .catch(err => {
+    console.error("Erreur lors de l'envoi vers Google Sheets :", err);
+  });
+}
